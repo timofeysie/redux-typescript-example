bf8dc0cd9c823782419281038524542251c35231
tim_curchod <timofeyc@hotmail.com>
2023-03-26 09:47:20 +0900

    formatted initial notification slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,13 +2,15 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import postsReducer from "../features/posts/postsSlice";
 import usersReducer from "../features/users/usersSlice";  
+import notificationsReducer from "../features/notifications/notificationsSlice"
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    posts: postsReducer,
-    users: usersReducer,
-  },
+    reducer: {
+        counter: counterReducer,
+        posts: postsReducer,
+        users: usersReducer,
+        notifications: notificationsReducer,
+    },
 });
 
 export type AppDispatch = typeof store.dispatch;
--- src/features/notifications/notificationsSlice.ts
+++ src/features/notifications/notificationsSlice.ts
@@ -1,33 +1,35 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-import { client } from '../../api/client'
+import { client } from "../../api/client";
 
 export const fetchNotifications = createAsyncThunk(
-  'notifications/fetchNotifications',
-  async (_, { getState }) => {
-    const allNotifications = selectAllNotifications(getState())
-    const [latestNotification] = allNotifications
-    const latestTimestamp = latestNotification ? latestNotification.date : ''
-    const response = await client.get(
-      `/fakeApi/notifications?since=${latestTimestamp}`
-    )
-    return response.data
-  }
-)
+    "notifications/fetchNotifications",
+    async (_, { getState }) => {
+        const allNotifications = selectAllNotifications(getState());
+        const [latestNotification] = allNotifications;
+        const latestTimestamp = latestNotification
+            ? latestNotification.date
+            : "";
+        const response = await client.get(
+            `/fakeApi/notifications?since=${latestTimestamp}`
+        );
+        return response.data;
+    }
+);
 
 const notificationsSlice = createSlice({
-  name: 'notifications',
-  initialState: [],
-  reducers: {},
-  extraReducers(builder) {
-    builder.addCase(fetchNotifications.fulfilled, (state, action) => {
-      state.push(...action.payload)
-      // Sort with newest first
-      state.sort((a, b) => b.date.localeCompare(a.date))
-    })
-  }
-})
+    name: "notifications",
+    initialState: [],
+    reducers: {},
+    extraReducers(builder) {
+        builder.addCase(fetchNotifications.fulfilled, (state, action) => {
+            state.push(...action.payload);
+            // Sort with newest first
+            state.sort((a, b) => b.date.localeCompare(a.date));
+        });
+    },
+});
 
-export default notificationsSlice.reducer
+export default notificationsSlice.reducer;
 
-export const selectAllNotifications = state => state.notifications
+export const selectAllNotifications = (state) => state.notifications;
